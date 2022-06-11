d8b120627c45f71ba04f6b7182c5c617f0de6ab8
Olamide Sholanke <olamide.sholanke@azenia.com>
2022-06-11 16:50:28 +0100

    fix: clean up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import DefaultLayout from "./components/layout/DefaultLayout";
 import Dashboard from "./pages/Dashboard";
 
@@ -8,7 +8,14 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Hello World</h1>} />
+        <Route
+          path="/"
+          element={
+            <Link to="/dashboard" style={{ padding: 20, display: "block" }}>
+              Click to Login to Dashboard
+            </Link>
+          }
+        />
         <Route path="dashboard" element={<DefaultLayout />}>
           <Route index element={<Dashboard />}></Route>
         </Route>
--- src/components/ui/SideBar.jsx
+++ src/components/ui/SideBar.jsx
@@ -6,42 +6,42 @@ export default function SideBar({ showSideBar }) {
   return (
     <div className="app-container__side-bar" data-show={showSideBar}>
       <div>
-        <Link to="/" className="app-container__side-bar__logo">
+        <Link to="/dashboard" className="app-container__side-bar__logo">
           <Icon.Logo />
         </Link>
 
         <div className="app-container__side-bar__main-links">
-          <Link to="/">
+          <Link to="/dashboard">
             <Icon.Home />
           </Link>
 
-          <Link to="/">
+          <Link to="/dashboard">
             <Icon.Receipt />
           </Link>
 
-          <Link to="/">
+          <Link to="/dashboard">
             <Icon.Building />
           </Link>
 
-          <Link to="/">
+          <Link to="/dashboard">
             <Icon.Profile />
           </Link>
 
-          <Link to="/">
+          <Link to="/dashboard">
             <Icon.Card />
           </Link>
 
-          <Link to="/">
+          <Link to="/dashboard">
             <Icon.Money />
           </Link>
 
-          <Link to="/">
+          <Link to="/dashboard">
             <Icon.Gift />
           </Link>
         </div>
       </div>
 
-      <Link to="/">
+      <Link to="/dashboard">
         <Icon.Home />
       </Link>
     </div>
